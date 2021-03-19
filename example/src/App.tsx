9a3e2871655a03fa800b0da1a987f14de9f006b2
Sehal Sein <seinsehal@gmail.com>
2021-03-19 13:45:29 +0530

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'acadamia-table-builder'
+import 'acadamia-table-builder/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
