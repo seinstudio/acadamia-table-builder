1cd82c5dce442aac50f5498869debfefd89c4d77
sehalsein <seinsehal@gmail.com>
2021-03-20 10:22:24 +0530

    Updates

---
+++ precache-manifest.a7b4781cabc46144e04fdf81eb190a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66726ce15cae464a8c1a6dc0fac3d643",
+    "url": "./index.html"
+  },
+  {
+    "revision": "45e4b1e4fecc9d86ef06",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "6756f69684ba2505d9e0",
+    "url": "./static/js/2.9d2d0832.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "./static/js/2.9d2d0832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45e4b1e4fecc9d86ef06",
+    "url": "./static/js/main.9c95c05f.chunk.js"
+  },
+  {
+    "revision": "b429cc1b95bb28d6dd3e",
+    "url": "./static/js/runtime-main.a17753cf.js"
+  }
+]);
